c335fbc4d98da36a205511ca3074288a483f2ba6
aaaangeln <aaaangeln01@gmail.com>
2023-12-05 17:07:56 +0300

    second commit

--- task3.js
+++ task3.js
@@ -1,100 +1,94 @@
 const crypto = require('crypto');
-const readline = require('readline');
-const key = crypto.randomBytes(32).toString('hex');
-const moves = process.argv.slice(2);
 
-function HMAC(key, data) {
-  const hmac = crypto.createHmac('sha256', key);
-  hmac.update(data);
+function generateHmac(hmacKey, move) {
+  const hmac = crypto.createHmac('sha256', hmacKey);
+  hmac.update(move);
   return hmac.digest('hex');
 }
 
-function Win(user, computer, moves) {
-  const half = Math.floor(moves.length / 2);
-  const index = moves.indexOf(user);
-  const win = moves.slice(index + 1, index + 1 + half);
-  const lose = moves.slice(index - half, index);
-  
-  if (win.includes(computer)) {
+function Computer(moves) {
+  const randomIndex = Math.floor(Math.random() * moves.length);
+  return moves[randomIndex];
+}
+
+function Win(User, Computer, moves) {
+  const indexUser = moves.indexOf(User);
+  const half = moves.length / 2;
+  const lose = moves.slice(indexUser + 1, indexUser + half + 1);
+  if (indexUser + half >= moves.length) {
+    lose.push(...moves.slice(0, half - lose.length));
+  }
+
+  if (lose.includes(Computer)) {
     return 'You WIN';
-  } else if (lose.includes(computer)) {
-    return 'You LOSE';
-  } else {
+  } else if (User === Computer) {
     return 'It is DRAW';
+  } else {
+    return 'You LOSE';
   }
 }
 
-function Help(moves) {
-    console.log('You choice:');
+function generateTable(moves) {
+  const table = [['Moves', ...moves]];
+  for (let i = 0; i < moves.length; i++) {
+    const row = [moves[i]];
+    for (let j = 0; j < moves.length; j++) {
+      if (i === j) {
+        row.push('DRAW');
+      } else if ((j - i + moves.length) % moves.length <= moves.length / 2) {
+        row.push('WIN');
+      } else {
+        row.push('LOSE');
+      }
+    }
+    table.push(row);
+  }
+  console.table(table);
+}
+
+
+function startGame() {
+  const moves = process.argv.slice(2);
+
+  if (moves.length < 3 || moves.length % 2 === 0 || new Set(moves).size !== moves.length) {
+    console.error('Invalid input >=3');
+    process.exit(1);
+  }
+
+  const hmacKey = crypto.randomBytes(32).toString('hex');
+  console.log(`HMAC key: ${hmacKey}`);
+
+  console.log('You choice:');
     moves.forEach((move, index) => {
       console.log(`${index + 1} - ${move}`);
     });
     console.log('0 - exit');
     console.log('? - help');
-  }
-
-function HelpTable(moves) {
-    const header = '+-------------+';
-    const separator = '|';
-    const line = '+-------------+';
-    const rowLength = moves.length;
-  
-    let table = `${header}${'-'.repeat(rowLength * 8)}+\n`;
-    table += `| v PC||User > ${separator}`;
-  
-    moves.forEach((move) => {
-      table += ` ${move} ${separator}`;
-    });
-    table += '\n';
-  
-    table += `${line}${'-'.repeat(rowLength * 8)}+\n`;
-  
-    moves.forEach((move1) => {
-      table += `| ${move1} ${separator}`;
-      moves.forEach((move2) => {
-        const result = Win(move1, move2, moves);
-        table += ` ${result} ${separator}`;
-      });
-      table += '\n';
-      table += `${line}${'-'.repeat(rowLength * 8)}+\n`;
-    });
-  
-    console.log(table);
-  }
-
-function Play(moves) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  console.log(`HMAC key: ${key}`);
-  Help(moves);
-
-  rl.on('line', (input) => {
-    if (input === '0') {
-      rl.close();
-    } else if (input === '?') {
-        Help(moves);
-    } else if (input >= 1 && input <= moves.length) {
-      const user = moves[input - 1];
-      const computer = moves[Math.floor(Math.random() * moves.length)];
-      const hmac = HMAC(key, computer);
-
-      console.log(`Your choice: ${user}`);
-      console.log(`Computer choice: ${computer}`);
-      console.log(Win(user, computer, moves));
-      console.log(`HMAC: ${hmac}`);
-      HelpTable(moves);
-      rl.close();
-    } else {
-      console.log('Invalid input');
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', () => {
+    let input = process.stdin.read();
+    if (input !== null) {
+      input = input.trim();
+      if (input === '0') {
+        process.exit(0);
+      } else if (input.toLowerCase() === 'help') {
+        console.log("You need to choose one of the options, after which the computer will make its choice, and you will see the result and a summary data table");
+      } else {
+        const choice = moves[input - 1];
+        if (choice) {
+          const computer = Computer(moves);
+          const hmac = generateHmac(hmacKey, computer);
+          console.log(`Computer: ${computer}`);
+          console.log(`HMAC: ${hmac}`);
+          console.log(Win(choice, computer, moves));
+          console.log(`HMAC key: ${hmacKey}`);
+          const table = generateTable(moves);
+        } else {
+          console.log('Invalid input. Please try again.');
+        }
+      }
     }
   });
 }
 
-if (moves.length < 3 || moves.length % 2 === 0) {
-  console.log('Please moves >=3');
-} else {
-  Play(moves);
-}
+startGame();
